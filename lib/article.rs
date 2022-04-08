d9edf728bb5684bb4caf16f5493d9d68da463405
tami5 <kkharji@protonmail.com>
2022-04-08 20:16:49 +0300

    feat: extra fields to article struct

--- lib/article.rs
+++ lib/article.rs
@@ -1,31 +1,37 @@
 mod collection;
 mod source;
 
-pub use collection::*;
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Deserializer, Serialize};
+
+pub use collection::*;
 pub use source::*;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Article {
     pub source: ArticleSource,
     pub author: Option<String>,
     pub title: String,
     // #[serde(default)]
-    #[serde(deserialize_with = "deserialize_description")]
+    #[serde(deserialize_with = "deserialize_string")]
     pub description: String,
+    #[serde(deserialize_with = "deserialize_string")]
+    pub content: String,
     pub url: String,
     #[serde(rename(deserialize = "urlToImage"))]
     pub url_to_image: Option<String>,
+    #[serde(rename(deserialize = "publishedAt"))]
+    pub published_at: DateTime<Utc>,
 }
 
 /// Custom deserializer so it doesn't fail when facing null and doesn't require me to have make it
 /// optional.
-fn deserialize_description<'de, D>(deserializer: D) -> Result<String, D::Error>
+fn deserialize_string<'de, D>(deserializer: D) -> Result<String, D::Error>
 where
     D: Deserializer<'de>,
 {
     Ok(match String::deserialize(deserializer) {
         Ok(str) => str,
-        Err(_) => "<<No Description>>".into(),
+        Err(_) => "".into(),
     })
 }
